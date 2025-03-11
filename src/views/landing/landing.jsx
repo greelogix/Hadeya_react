9317a3eabe581045d544e02ce5a1327617a18885
zain-here777 <zainii.cs@gmail.com>
2025-03-11 13:25:17 +0600

    latest

--- src/views/landing/landing.jsx
+++ src/views/landing/landing.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
+import 'src/assets/css/views/landing/landing.scss'
 
 const Landing = (props) => {
   return <div className="">Landing Page</div>;
