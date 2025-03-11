ecde2c2cbb6b82198747335ed0d8435096b35e4c
tayyab <149600417+abuzer-social@users.noreply.github.com>
2025-03-11 12:58:28 +0500

    test commit

--- src/views/register/login/login.jsx
+++ src/views/register/login/login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
     <div className="  container-login row">
       <div className="col-12   background-color  d-flex flex-column form-container  justify-content-between">
         <div className=" ">
-          <div className="row justify-content-center mt-5 text-center">
+          <div className="row justify-content-center mt-5  text-center">
             <div className=" col-4  mb-5   "></div>
           </div>
         </div>
