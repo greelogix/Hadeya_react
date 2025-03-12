a1a4f38d599c571c6a79cea83d9f84dac2cf5a8c
tayyab <149600417+abuzer-social@users.noreply.github.com>
2025-03-12 13:58:38 +0500

    wip:landing page

--- src/container/layout/navBar/navBar.jsx
+++ src/container/layout/navBar/navBar.jsx
@@ -14,41 +14,10 @@ const NavBar = ({ children }) => {
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg z-index-1 fixed-top bg-body-tertiary  nav-color nav-style">
-        <div className="container-fluid">
-          <div class="btn-group right-align">
-            <button
-              type="button"
-              class="btn btn-secondary dropdown-toggle custom-button"
-              data-bs-toggle="dropdown"
-              data-bs-display="static"
-              aria-expanded="false"
-            >
-              <img src={useicon} alt="" width="22px" height="26px" />
-              <span className="ms-1 color-black"> Developer</span>
-            </button>
-            <ul class="dropdown-menu dropdown-menu-end ">
-              <li>
-                <button class="dropdown-item" type="button">
-                  <i class="fa-solid fa-user me-1"></i>{" "}
-                  <span>User Profile</span>
-                </button>
-              </li>
-              <li>
-                <button
-                  class="dropdown-item"
-                  type="button"
-                  onClick={handleLogout}
-                >
-                  <i class="fa-solid fa-right-from-bracket me-1"></i>
-                  <span> Logout</span>
-                </button>
-              </li>
-            </ul>
-          </div>
-        </div>
+      <nav className="navbar navbar-expand-lg z-index-1   bg-body-tertiary  nav-color nav-style">
+        <div className="container-fluid"></div>
       </nav>
-      <main>{children}</main>
+      <main className="mt-1">{children}</main>
     </>
   );
 };
--- src/views/landing/landing.jsx
+++ src/views/landing/landing.jsx
@@ -1,14 +1,57 @@
-import React, { useState, useEffect } from "react";
-import { connect } from "react-redux";
-import 'src/assets/css/views/landing/landing.scss'
+import img_bg from "../../assets/images/landing-hero-bg.jpeg";
+import about_vector from "../../assets/images/about-vector.png";
+import "../../assets/css/views/landing/landing.scss";
 
 const Landing = (props) => {
-  return <div className="">Landing Page</div>;
+  return (
+    <div className="main-container d-flex flex-coulmn justify-content-center">
+      <div className="width-90">
+        <div className="hero-container position-relative mt-5">
+          <div className="hero-text-container gap-4 pb-5 row col-8 position-absolute">
+            <div className="col-5">
+              {" "}
+              <p className="heading">Bring Dreams to Life</p>
+            </div>
+
+            <div className="col-6 px-4">
+              <p className="description mt-4">
+                Create, share, and contribute to wishlists for yourself and
+                others. Together, let’s make dreams a reality.
+              </p>
+              <button className="btn  w-100">Get Started</button>
+            </div>
+          </div>
+          <div className="hero-container-inner">
+            <img src={img_bg} alt="" />
+          </div>
+        </div>
+        <div class="about-section">
+          <div class="">
+            <div class="align-items-center justify-content-between row">
+              <div class="col-lg-4  ">
+                <div className="mb-5 mb-md-0 line-container m-auto text-centerline-container m-auto m-md-0 text-center text-md-start">
+                  <h2 class="title">About Hadeya</h2>
+                  <div class="title-line mt-3">
+                    <span class="dot"></span>
+                  </div>
+                  <img src={about_vector} className="pt-3" alt="" />
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <p class="description">
+                  Hadeya is a platform designed to connect people through
+                  wishlists. Whether you want to save for your dreams or
+                  contribute to someone else's, Hadeya makes it easy, fun, and
+                  collaborative. From gadgets to education, we’re here to help
+                  you achieve and give back.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
-const mapStateToProps = (state) => ({
-  // brandsState: state.brandsReducer,
-});
-const mapDispatchToProps = (dispatch) => ({
-  // getBrands: () => dispatch(brandsActions.getBrands()),
-});
-export default connect(mapStateToProps, mapDispatchToProps)(Landing);
+
+export default Landing;
